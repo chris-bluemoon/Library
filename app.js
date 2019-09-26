fa9265c382b1b95f2aa79f0addc1e74166192dea
Christopher Milner <chris.milner@gmail.com>
2019-09-26 16:46:29 +0800

    Added books.ejs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -14,8 +16,60 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Niko Tolstoy',
+    read: false
+  },
+  {
+    title: 'Saw and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Niko Tolstoy',
+    read: false
+  },
+  {
+    title: 'Par and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Niko Tolstoy',
+    read: false
+  },
+  {
+    title: 'Car and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Niko Tolstoy',
+    read: false
+  }
+];
+
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        title: 'Library',
+        books
+      });
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { list: ['d', 'b'], title: 'Library' });
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
 });
 
 app.listen(port, () => {
